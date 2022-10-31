bb0ee51437e2a4bd2ed416f4c2fc01f47b0cd009
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-10-31 14:37:29 +0100

    feat: init data access object lib

--- dao/src/lib.rs
+++ dao/src/lib.rs
@@ -1,10 +1,35 @@
-
+pub mod db_client;
+pub mod env_utils;
+use crate::env_utils::env_utils::{ load_env_file, get_env_var, get_full_db_path };
+use crate::db_client::db_client::{ get_db_connection, TestRun };
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_loads_env_file() {
+        let vars = load_env_file();
+        assert_eq!(
+            vars.get(&"RPC_URL".to_string()),
+            Some(&"https://mainnet.aurora.dev:443/".to_string())
+        );
+        assert_eq!(
+            vars.get(&"NETWORK_NAME".to_string()),
+            Some(&"mainnet_aurora_plus".to_string())
+        )
+    }
+
+    #[test]
+    fn it_loads_test_runs() {
+        let mut network_name = get_env_var(&"NETWORK_NAME".to_string())
+            .unwrap_or("mainnet_aurora_plus".to_string());
+        let full_db_path = get_full_db_path().unwrap();
+        let conn = get_db_connection(&full_db_path).unwrap();
+        let runs_table = get_env_var(&"RUNS_TABLE".to_string())
+            .unwrap_or("aurora_relayer_test_runs".to_string());
+        let test_run = TestRun::new(&conn, network_name, runs_table).unwrap();
+        assert_eq!(test_run.db_id, 20);
+        network_name = get_env_var(&"NETWORK_NAME".to_string()).unwrap();
+        assert_eq!(test_run.network, network_name);
     }
 }
