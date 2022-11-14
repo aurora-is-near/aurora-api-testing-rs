9802c3f52f0f31dc6c75b7142e87c08de74ca7b2
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-14 12:58:18 +0100

    feat: `eth_getFilterLogs`

---
+++ dynamic_apis_tests/tests/eth_get_filter_logs.rs
@@ -0,0 +1,25 @@
+use jsonrpsee_core::client::ClientT;
+use jsonrpsee_core::rpc_params;
+use jsonrpsee_http_client as http_client;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[path = "configs.rs"]
+mod configs;
+use configs::Configs;
+
+#[tokio::test]
+async fn test_eth_get_code() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber);
+    let configs = Configs::load().unwrap();
+    let client = http_client::HttpClientBuilder::default().build(configs.rpc_url)?;
+    let params = rpc_params!["0x0"];
+    let response: Result<Vec<String>, jsonrpsee_core::Error> =
+        client.request("eth_getFilterLogs", params).await;
+    let res = response;
+    info!("filter changes: {:?}", res); // no filters
+    Ok(())
+}
