c3c09e2191e4d5844db56d77d975f59e772c5989
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-23 16:24:07 +0100

    feat: add `AuroraTransactionReceipt`

---
+++ dynamic_apis_tests/tests/aurora_transaction_receipt.rs
@@ -0,0 +1,36 @@
+use dao::dao::helpers::Address;
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all(serialize = "snake_case", deserialize = "camelCase"))]
+pub struct Log {
+    pub transaction_index: String,
+    pub block_number: String,
+    pub transaction_hash: String,
+    pub address: String,
+    pub topics: Vec<String>,
+    pub data: String,
+    pub log_index: String,
+    pub block_hash: String,
+    pub removed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all(serialize = "snake_case", deserialize = "camelCase"))]
+pub struct AuroraTransactionReceipt {
+    pub from: Address,
+    pub to: Address,
+    pub contract_address: Option<Address>,
+    pub transaction_index: String,
+    pub gas_used: String,
+    pub logs_bloom: String,
+    pub block_hash: String,
+    pub transaction_hash: String,
+    pub logs: Vec<Log>,
+    pub block_number: String,
+    pub cumulative_gas_used: String,
+    pub status: String,
+    pub near_receipt_hash: String,
+    pub near_transaction_hash: String,
+}
