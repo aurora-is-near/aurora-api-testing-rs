6a901c56bb0bc046d0ab0e525dfb6a33163819fe
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-24 11:51:43 +0100

    feat:  `eth_protocolVersion`

---
+++ dynamic_apis_tests/tests/eth_new_pending_transaction_filter.rs
@@ -0,0 +1,12 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::test]
+async fn test_eth_new_pending_transaction_filter() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    let _t = tracing::subscriber::set_global_default(subscriber);
+    info!("eth_newPendingTransactionFilter is not supported in Aurora");
+    Ok(())
+}
