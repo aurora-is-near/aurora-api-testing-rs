8bfe8513ac3c9440b344d88c3d3d68c498188066
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-24 10:45:23 +0100

    feat: `eth_getUncleCountByBlockNumber`, `eth_getUncleCountByBlockHash`, `eth_getUncleBlockByNumberAndIndex`, `eth_getUncleBlockByHashAndIndex`

--- dynamic_apis_tests/tests/eth_get_uncle_count_by_block_number.rs
+++ dynamic_apis_tests/tests/eth_get_uncle_count_by_block_number.rs
@@ -2,11 +2,11 @@ use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::test]
-async fn test_eth_get_uncle_by_block_hash_and_index() -> anyhow::Result<()> {
+async fn test_eth_get_uncle_count_by_block_number() -> anyhow::Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .finish();
     let _t = tracing::subscriber::set_global_default(subscriber);
-    info!("eth_getUncleBlockByHashAndIndex is not supported in Aurora");
+    info!("eth_getUncleCountByBlockNumber is not supported in Aurora");
     Ok(())
 }
