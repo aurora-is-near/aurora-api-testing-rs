0c1eea17946b2eed0338597fe8126c86afa428f7
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-24 11:59:09 +0100

    feat: `eth_syncing`

--- dynamic_apis_tests/tests/eth_syncing.rs
+++ dynamic_apis_tests/tests/eth_syncing.rs
@@ -1,19 +1,15 @@
 use jsonrpsee_core::client::ClientT;
 use jsonrpsee_core::rpc_params;
 use jsonrpsee_http_client as http_client;
-use tracing::{info, Level};
+use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 #[path = "configs.rs"]
 mod configs;
 use configs::Configs;
 
-#[path = "utils.rs"]
-mod utils;
-use utils::hex_string_to_i32;
-
 #[tokio::test]
-async fn test_eth_protocol_version() -> anyhow::Result<()> {
+async fn test_eth_syncing() -> anyhow::Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .finish();
@@ -21,9 +17,7 @@ async fn test_eth_protocol_version() -> anyhow::Result<()> {
     let configs = Configs::load().unwrap();
     let client = http_client::HttpClientBuilder::default().build(configs.rpc_url)?;
     let params = rpc_params![];
-    let response: Result<String, _> = client.request("eth_protocolVersion", params).await;
-    let protocol_version = hex_string_to_i32(response.unwrap());
-    assert_eq!(protocol_version, 65);
-    info!("eth_protocolVersion is: {}", protocol_version);
+    let response: Result<bool, _> = client.request("eth_syncing", params).await;
+    assert_eq!(response.unwrap(), false);
     Ok(())
 }
