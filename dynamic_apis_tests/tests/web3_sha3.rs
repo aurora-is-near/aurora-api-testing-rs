99c6985c27eab4dcf9e0b6c7350465ad7bdb82e5
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-11-30 12:06:54 +0100

    fix: `web3_sha3` testing

--- dynamic_apis_tests/tests/web3_sha3.rs
+++ dynamic_apis_tests/tests/web3_sha3.rs
@@ -1,5 +1,12 @@
+use jsonrpsee_core::client::ClientT;
+use jsonrpsee_core::rpc_params;
+use jsonrpsee_http_client as http_client;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+
+#[path = "configs.rs"]
+mod configs;
+use configs::Configs;
 
 #[tokio::test]
 async fn test_web3_sha3() -> anyhow::Result<()> {
@@ -7,6 +14,16 @@ async fn test_web3_sha3() -> anyhow::Result<()> {
         .with_max_level(Level::INFO)
         .finish();
     let _t = tracing::subscriber::set_global_default(subscriber);
-    info!("web3_sha3 is not supported in Aurora");
+    let configs = Configs::load().unwrap();
+    let client = http_client::HttpClientBuilder::default().build(configs.rpc_url)?;
+    let params = rpc_params!["0x68656c6c6f20776f726c64"];
+    let response: Result<String, _> = client.request("web3_sha3", params).await;
+    let result = response.unwrap();
+    // web3_sha3
+    assert_eq!(
+        result.clone(),
+        "0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad"
+    );
+    info!("web3_sha3 output is: {:?}", result);
     Ok(())
 }
