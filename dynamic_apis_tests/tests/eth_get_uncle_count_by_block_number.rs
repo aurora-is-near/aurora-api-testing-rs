cf717ad85aed855f5e90810c3bac93d72faca4f0
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-12-02 14:11:28 +0100

    fix: `eth_getUncleBy*` and `eth_newFilter`

--- dynamic_apis_tests/tests/eth_get_uncle_count_by_block_number.rs
+++ dynamic_apis_tests/tests/eth_get_uncle_count_by_block_number.rs
@@ -1,12 +1,44 @@
+use dao::dao::helpers::TransactionReceipt;
+use dao::dao::models::{TestRun, TestTask};
+use jsonrpsee_core::client::ClientT;
+use jsonrpsee_core::rpc_params;
+use jsonrpsee_http_client as http_client;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
+#[path = "configs.rs"]
+mod configs;
+use configs::Configs;
+
+#[path = "utils.rs"]
+mod utils;
+use utils::hex_string_to_i32;
+
 #[tokio::test]
 async fn test_eth_get_uncle_count_by_block_number() -> anyhow::Result<()> {
+    let configs = Configs::load().unwrap();
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .finish();
-    let _t = tracing::subscriber::set_global_default(subscriber);
-    info!("eth_getUncleCountByBlockNumber is not supported in Aurora");
+    let _ = tracing::subscriber::set_global_default(subscriber);
+    let test_run = TestRun::new(&configs.conn, configs.network, configs.runs_table).unwrap();
+    let task: TestTask = test_run
+        .filter_tasks_with_limit_one("transferNtimes".to_string())
+        .unwrap();
+    let data_contents: Vec<String> = task
+        .get_test_data_content_array("receipt".to_string())
+        .unwrap();
+    let receipts = TransactionReceipt::load(data_contents).unwrap();
+    let client = http_client::HttpClientBuilder::default().build(configs.rpc_url)?;
+    for i in 0..receipts.len() {
+        info!("block number: {}", receipts[i].block_number);
+        let params = rpc_params![receipts[i].block_number.to_string(), true];
+        let response: Result<String, _> = client
+            .request("eth_getUncleCountByBlockNumber", params)
+            .await;
+        let uncle_count = response.unwrap();
+        assert_eq!(hex_string_to_i32(uncle_count.clone()), 0);
+        info!("eth_getUncleCountByBlockNumber: {:?}", uncle_count.clone());
+    }
     Ok(())
 }
