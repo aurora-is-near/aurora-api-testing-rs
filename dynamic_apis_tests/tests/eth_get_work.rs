adc046801f6a038daa2e9050a722698664aa85bd
Diego Figueroa <diego.figueroa@aurora.dev>
2024-05-08 13:36:25 -0400

    test: revert commenting of tests

--- dynamic_apis_tests/tests/eth_get_work.rs
+++ dynamic_apis_tests/tests/eth_get_work.rs
@@ -9,40 +9,41 @@ use tracing_subscriber::FmtSubscriber;
 mod configs;
 use configs::Configs;
 
-// #[tokio::test]
-// async fn test_eth_get_work() -> anyhow::Result<()> {
-//     let subscriber = FmtSubscriber::builder()
-//         .with_max_level(Level::INFO)
-//         .finish();
-//     let _ = tracing::subscriber::set_global_default(subscriber);
-//     let configs = Configs::load().unwrap();
-//     let params = rpc_params![];
-//     let client = http_client::HttpClientBuilder::default().build(configs.rpc_url.clone())?;
-//     let response: Result<String, jsonrpsee_core::Error> =
-//         client.request("eth_getWork", params).await;
-//     if configs.rpc_url.clone().contains("aurora") {
-//         let result = match response {
-//             Ok(value) => value,
-//             Err(error) => error.to_string(),
-//         };
-//         // info!("{}", result);
-//         assert_eq!(result.contains("Unsupported method: eth_getWork"), true);
-//     } else if configs.rpc_url.clone().contains("goerli") {
-//         let goerli_responses = [
-//             "the method eth_getWork does not exist",
-//             "method not supported",
-//             "the method eth_getWork does not exist",
-//         ];
-//         let result = match response {
-//             Ok(value) => value,
-//             Err(error) => error.to_string(),
-//         };
-//         // info!("{}", result);
-//         let does_exist: Vec<bool> = goerli_responses
-//             .iter()
-//             .map(|v| result.contains(&v.to_string()))
-//             .collect();
-//         info!("Unsupported method ? {:?}", does_exist.contains(&true));
-//     }
-//     Ok(())
-// }
+#[tokio::test]
+#[ignore = "unsupported method"]
+async fn test_eth_get_work() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    let _ = tracing::subscriber::set_global_default(subscriber);
+    let configs = Configs::load().unwrap();
+    let params = rpc_params![];
+    let client = http_client::HttpClientBuilder::default().build(configs.rpc_url.clone())?;
+    let response: Result<String, jsonrpsee_core::Error> =
+        client.request("eth_getWork", params).await;
+    if configs.rpc_url.clone().contains("aurora") {
+        let result = match response {
+            Ok(value) => value,
+            Err(error) => error.to_string(),
+        };
+        // info!("{}", result);
+        assert_eq!(result.contains("Unsupported method: eth_getWork"), true);
+    } else if configs.rpc_url.clone().contains("goerli") {
+        let goerli_responses = [
+            "the method eth_getWork does not exist",
+            "method not supported",
+            "the method eth_getWork does not exist",
+        ];
+        let result = match response {
+            Ok(value) => value,
+            Err(error) => error.to_string(),
+        };
+        // info!("{}", result);
+        let does_exist: Vec<bool> = goerli_responses
+            .iter()
+            .map(|v| result.contains(&v.to_string()))
+            .collect();
+        info!("Unsupported method ? {:?}", does_exist.contains(&true));
+    }
+    Ok(())
+}
--- dynamic_apis_tests/tests/eth_uninstall_filter.rs
+++ dynamic_apis_tests/tests/eth_uninstall_filter.rs
@@ -25,30 +25,31 @@ impl FilterParams {
     }
 }
 
-// #[tokio::test]
-// async fn test_eth_uninstall_filter() -> anyhow::Result<()> {
-//     let subscriber = FmtSubscriber::builder()
-//         .with_max_level(Level::INFO)
-//         .finish();
-//     let _t = tracing::subscriber::set_global_default(subscriber);
-//     let configs = Configs::load().unwrap();
-//     let client = http_client::HttpClientBuilder::default().build(configs.rpc_url.clone())?;
-//     let filter_id: String;
-//     if configs.rpc_url.contains("aurora") {
-//         let params = rpc_params![];
-//         let tx_pending_filter_id: Result<String, _> = client
-//             .request("eth_newPendingTransactionFilter", params)
-//             .await;
-//         filter_id = tx_pending_filter_id.unwrap();
-//     } else {
-//         let params = rpc_params![FilterParams::new("0x0".to_string(), "0x1".to_string())];
-//         let new_filter_id: Result<String, _> = client.request("eth_newFilter", params).await;
-//         filter_id = new_filter_id.unwrap();
-//     }
-//     let params = rpc_params![filter_id];
-//     let response: Result<bool, _> = client.request("eth_uninstallFilter", params).await;
-//     let status = response.unwrap();
-//     assert_eq!(status, true);
-//     info!("eth_uninstallFilter status: {}", status);
-//     Ok(())
-// }
+#[tokio::test]
+#[ignore = "unsupported method"]
+async fn test_eth_uninstall_filter() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    let _t = tracing::subscriber::set_global_default(subscriber);
+    let configs = Configs::load().unwrap();
+    let client = http_client::HttpClientBuilder::default().build(configs.rpc_url.clone())?;
+    let filter_id: String;
+    if configs.rpc_url.contains("aurora") {
+        let params = rpc_params![];
+        let tx_pending_filter_id: Result<String, _> = client
+            .request("eth_newPendingTransactionFilter", params)
+            .await;
+        filter_id = tx_pending_filter_id.unwrap();
+    } else {
+        let params = rpc_params![FilterParams::new("0x0".to_string(), "0x1".to_string())];
+        let new_filter_id: Result<String, _> = client.request("eth_newFilter", params).await;
+        filter_id = new_filter_id.unwrap();
+    }
+    let params = rpc_params![filter_id];
+    let response: Result<bool, _> = client.request("eth_uninstallFilter", params).await;
+    let status = response.unwrap();
+    assert_eq!(status, true);
+    info!("eth_uninstallFilter status: {}", status);
+    Ok(())
+}
